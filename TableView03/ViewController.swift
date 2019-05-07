f9905bd080914f26fd9e9aa0e361467dfdd5fbb7
Jonghyun Kim <jhkim@dit.ac.kr>
2019-05-07 15:12:20 +0900

    03 simple TableView

--- TableView03/ViewController.swift
+++ TableView03/ViewController.swift
@@ -16,6 +16,12 @@ class ViewController: UIViewController, UITableViewDataSource {
     var animals = ["Cat", "Dog", "Pig", "Bird", "Whale", "Cow"]
     var detail = ["고양이", "강아지", "돼지", "새", "고래", "소"]
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        myTableView.dataSource = self
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return animals.count
     }
@@ -33,13 +39,5 @@ class ViewController: UIViewController, UITableViewDataSource {
         
     }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        myTableView.dataSource = self
-    }
-
-
 }
 
